509c5b9f5986f80f4bce328d7146f27c5c76cac3
dinasayedddd <ds650009@gmail.com>
2025-10-29 22:20:40 +0300

    Updates

--- src/Componts/Notfound/Notfound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Notfound() {
-  return (
-    <>
-      <div class="text-center py-5"> <h1 class="display-1">404</h1> <h2>File not found</h2> </div>
-    </>
-  )
-}
